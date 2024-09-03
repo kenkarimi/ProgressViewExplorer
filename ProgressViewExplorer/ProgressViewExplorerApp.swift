53b9cf4bb607dc3175f1fe99c5f029f80852baea
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-09-04 00:50:59 +0300

    Initial Commit

---
+++ ProgressViewExplorer/ProgressViewExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewExplorerApp.swift
+//  ProgressViewExplorer
+//
+//  Created by Kennedy Karimi on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
